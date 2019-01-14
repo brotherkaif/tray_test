7c9d0975c10a128f154433d138eda6af99f21cda
Kaif Ahmed <hello@kaif.me>
2019-01-14 22:20:46 +0000

    Input file is loading correctly and has been split out into an array for further processing.

--- robovac.js
+++ robovac.js
@@ -1,7 +1,20 @@
-document.getElementById('loadInput').addEventListener('click', getInput)
-
-function getInput() {
+function init() {
   fetch('input.txt')
-    .then(response => response.text())
-    .then(text => console.log(text));
+  .then(response => response.text())
+  .then(text => processInput(text))
+  .catch(err => console.log(err));
 }
+
+function processInput(message) {
+  inputString = message;
+  inputArray = message.split("\n");
+}
+
+let inputString;
+let inputArray;
+init();
+
+// Get initial grid size from inputArray[0]
+// Get robot position from inputArray[1]
+// Get the direction from inputArray[inputArray.length-1]
+// Get dirty tiles from inputArray[2]..inputArray[inputArray.length-1]
