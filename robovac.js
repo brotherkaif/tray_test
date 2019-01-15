7688563acf0dadf5ca98764d884e17a97f761e99
Kaif Ahmed <hello@kaif.me>
2019-01-15 23:17:35 +0000

    Refactoring complete.

--- robovac.js
+++ robovac.js
@@ -1,21 +1,25 @@
+let gridSize;
+let robotPosition;
+let dirtyTiles;
+let robotRoute;
+
 function loadInput() {
-  return "5 5\n1 2\n1 0\n2 2\n2 3\nNNESEESWNWW"
+  return '5 5\n1 2\n1 0\n2 2\n2 3\nNNESEESWNWW';
 }
 
 function processInput(input) {
-
   // Convert string into an array for easier processing
-  let inputArray = input.split("\n");
-  
+  const inputArray = input.split('\n');
+
   // Get grid size
   gridSize = inputArray[0].split(' ').map(x => parseInt(x, 10));
-  
+
   // Get robot position
   robotPosition = inputArray[1].split(' ').map(x => parseInt(x, 10));
-  
+
   // Build array of dirty tiles
   dirtyTiles = inputArray.slice(2, inputArray.length - 1);
-  
+
   // Get the path the robot will take
   robotRoute = inputArray[inputArray.length - 1].split('');
 }
@@ -24,69 +28,67 @@ function moveBot(direction) {
   // Take a direction and move the bot accordingly
   // Also check if robot is currently at edge
   switch(direction) {
-    case "N":
-    if (robotPosition[1] < (gridSize[1] - 1)) {
-      robotPosition[1] += 1;
-    }
-    break;
-    case "E":
-    if (robotPosition[0] > 0) {
-      robotPosition[0] -= 1;
-    }
-    break;
-    case "S":
-    if (robotPosition[1] > 0) {
-      robotPosition[1] -= 1;
-    }
-    break;
-    case "W":
-    if (robotPosition[0] < (gridSize[0] - 1)) {
-      robotPosition[0] += 1;
-    }
-    break; 
+    case 'N':
+      if (robotPosition[1] < (gridSize[1] - 1)) {
+        robotPosition[1] += 1;
+      }
+      break;
+    case 'E':
+      if (robotPosition[0] > 0) {
+        robotPosition[0] -= 1;
+      }
+      break;
+    case 'S':
+      if (robotPosition[1] > 0) {
+        robotPosition[1] -= 1;
+      }
+      break;
+    case 'W':
+      if (robotPosition[0] < (gridSize[0] - 1)) {
+        robotPosition[0] += 1;
+      }
+      break;
     default:
-    console.log(`${direction} is not a valid direction`);
+      console.log(`${direction} is not a valid direction`);
   }
 }
 
-function calculateCleaned(routeTaken, dirtyTiles) {
-  // Remove duplicate tiles from tilesVisited to avoid double counting
-  let tilesVisited = routeTaken.filter((v,i) => routeTaken.indexOf(v) === i);
-  let tilesCleaned = 0;
-  
-  // Double iteration required to compare arrays
-  tilesVisited.forEach((visited) => {
-    dirtyTiles.forEach((dirty) => {
-      if (visited == dirty) {
-        tilesCleaned += 1;
-      }
-    });
-  });
-  
-  return tilesCleaned;
-}
-
 function runBot() {
-  let routeTaken = [];
-  
-  routeTaken.push(robotPosition.join(" "));
+  const routeTaken = [];
+
+  routeTaken.push(robotPosition.join(' '));
   robotRoute.forEach((direction) => {
     moveBot(direction);
-    routeTaken.push(robotPosition.join(" "));
+    routeTaken.push(robotPosition.join(' '));
   });
-  
+
   return routeTaken;
 }
 
-let gridSize;
-let robotPosition;
-let dirtyTiles;
-let robotRoute;
-let input = loadInput();
+function calculateCleaned(route, dirty) {
+  // Remove duplicate tiles from tilesVisited to avoid double counting
+  const visited = route.filter((v, i) => route.indexOf(v) === i);
+  let cleaned = 0;
+
+  // Double iteration required to compare arrays
+  visited.forEach((location) => {
+    dirty.forEach((spot) => {
+      if (location === spot) {
+        cleaned += 1;
+      }
+    });
+  });
+
+  return cleaned;
+}
+
+// Load input and calculate global vars
+const input = loadInput();
 processInput(input);
 
-let routeTaken = runBot();
+// runBot along route and get the route taken
+const routeTaken = runBot();
 
 // Final output as per spec
-console.log(robotPosition.join(" "));
+console.log(robotPosition.join(' '));
 console.log(calculateCleaned(routeTaken, dirtyTiles));
